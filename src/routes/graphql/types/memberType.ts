5c05f24cfbdfb6420e15a1df03ba384ac09b3744
AndreevSV <andreevsv@gmail.com>
2025-05-25 13:33:34 +0300

    refactor: change imports in memberType and remove unnecessary CreateUserInput that was moved prevously in InputTypes

--- src/routes/graphql/types/memberType.ts
+++ src/routes/graphql/types/memberType.ts
@@ -1,28 +1,24 @@
-import {GraphQLFloat, GraphQLString, GraphQLEnumType, GraphQLInt, GraphQLList, GraphQLNonNull, GraphQLObjectType, GraphQLInputObjectType } from 'graphql';
-import {UUIDType} from './uuid.js';
-import {Profile} from './profileType.js';
+import {
+  GraphQLFloat,
+  GraphQLEnumType,
+  GraphQLInt,
+  GraphQLNonNull,
+  GraphQLObjectType,
+} from 'graphql';
 
 export const MemberTypeId = new GraphQLEnumType({
-    name: 'MemberTypeId',
-    values: {
-        BASIC: {value: 'BASIC'},
-        BUSINESS: {value: 'BUSINESS'},
-    }
+  name: 'MemberTypeId',
+  values: {
+    BASIC: { value: 'BASIC' },
+    BUSINESS: { value: 'BUSINESS' },
+  },
 });
 
 export const MemberType = new GraphQLObjectType({
-    name: 'MemberType', 
-    fields: () => ({
-        id: {type: new GraphQLNonNull(MemberTypeId)},
-        discount: {type: GraphQLFloat},
-        postsLimitPerMonth: {type: GraphQLInt},
-    }),
-});
-
-export const CreateUserInput = new GraphQLInputObjectType({
-    name: 'CreateUserInput',
-    fields: () => ({
-        name: {type: GraphQLString},
-        balance: {type: GraphQLFloat},
-    }),
+  name: 'MemberType',
+  fields: () => ({
+    id: { type: new GraphQLNonNull(MemberTypeId) },
+    discount: { type: GraphQLFloat },
+    postsLimitPerMonth: { type: GraphQLInt },
+  }),
 });
